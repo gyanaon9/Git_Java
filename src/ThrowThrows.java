983d866b9022abd7aaade615f97728e13b0688f8
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2018-08-15 22:50:42 +0530

    byt rrr

--- src/ThrowThrows.java
+++ src/ThrowThrows.java
@@ -1,13 +1,13 @@
 
 public class ThrowThrows {
 
-	//@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
 		try {
 			int a = 10 / 5;
 			System.out.println(a);
 			if (false)
-				throw new Exception("Hey");
+				throw new Exception("Hello World");
 		} catch (Exception e) {
 			System.out.println("Hey");
 		} finally {
